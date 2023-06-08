9ac1f4a9b509621109cc4e3b82ebd24045875fbb
vahamad <vaseemahamad2@gmail.com>
2023-06-08 11:31:01 +0530

    innitaize variable to test demo

--- app/src/main/java/com/example/fileuploadnew/MainActivity.java
+++ app/src/main/java/com/example/fileuploadnew/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.fileuploadnew;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test=findViewById(R.id.test);
+        test.setText("this is test");
     }
 }
