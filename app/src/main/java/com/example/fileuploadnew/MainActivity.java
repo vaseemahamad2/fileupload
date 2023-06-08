fb3b352e7a5bae7619038cd6d2afb9d979b65bc7
vahamad <vaseemahamad2@gmail.com>
2023-06-08 11:46:56 +0530

    thi is new test

--- app/src/main/java/com/example/fileuploadnew/MainActivity.java
+++ app/src/main/java/com/example/fileuploadnew/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         test=findViewById(R.id.test);
         test.setText("this is test");
+        test.setText("this is new test");
+
     }
 }
